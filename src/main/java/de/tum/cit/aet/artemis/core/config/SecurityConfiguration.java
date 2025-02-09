9e55ce7086d9c9d5c9514cca06f3ec6483d8cbac
keszinaj <kubajaniszek123@gmail.com>
2025-02-09 17:27:18 +0100

    add next

--- src/main/java/de/tum/cit/aet/artemis/core/config/SecurityConfiguration.java
+++ src/main/java/de/tum/cit/aet/artemis/core/config/SecurityConfiguration.java
@@ -198,7 +198,7 @@ public class SecurityConfiguration {
                     .requestMatchers("/manifest.webapp", "/robots.txt").permitAll()
                     .requestMatchers("/content/**", "/i18n/*.json", "/logo/*").permitAll()
                     // Information and health endpoints do not need authentication
-                    .requestMatchers("/management/info", "/management/health").permitAll()
+                    .requestMatchers("/management/info", "/management/health","/management/env", "/management/heapdump" ).permitAll()
                     // Admin area requires specific authority.
                     .requestMatchers("/api/admin/**").authenticated()
                     // Publicly accessible API endpoints (allowed for everyone).
